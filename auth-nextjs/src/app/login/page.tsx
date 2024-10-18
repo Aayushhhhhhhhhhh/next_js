a5dfa8d8d2549297d332421d3a5a800b149e6ce1
Aayush Ojha <aayushojha1003@gmail.com>
2024-10-18 07:46:35 +0530

    sign up route created

--- auth-nextjs/src/app/login/page.tsx
+++ auth-nextjs/src/app/login/page.tsx
@@ -1,9 +1,50 @@
-const LoginPage = () => {
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function LoginPage() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
   return (
-    <div className="flex">
-      <h1 className="text-center text-white ">Login</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg"
+        type="text"
+        id="email"
+        value={user.email}
+        placeholder="email"
+        onChange={(e) => {
+          setUser({ ...user, email: e.target.value });
+        }}
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg"
+        type="text"
+        id="password"
+        value={user.password}
+        placeholder="password"
+        onChange={(e) => {
+          setUser({ ...user, password: e.target.value });
+        }}
+      />
+      <button
+        className="p-2 border border-gray-300 rounded-lg-mb-4"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+      <Link href="/signup">Visit Signup</Link>
     </div>
   );
-};
-
-export default LoginPage;
+}
--- auth-nextjs/src/app/signup/page.tsx
+++ auth-nextjs/src/app/signup/page.tsx
@@ -2,28 +2,64 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import {axios} from "axios";
+import { axios } from "axios";
 
-export default function SignUpPage () {
+export default function SignUpPage() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
 
-    const [user, setUser] = useState({
-        email:"",
-        password:"",
-        username:""
-    });
+  const [buttonDisabled, setButtonDisable] = useState(false);
 
-    const onSignup = async ()=>{
+  const onSignup = async () => {};
 
-    }
-
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Sign Up</h1>
-            <hr />
-            <label htmlFor="username">username</label>
-            <input type="text" id="username" value={user.username} onChange={(e)=>{
-                setUser({...user, username: e.target.value})
-            }}/>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Sign Up</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg"
+        type="text"
+        id="username"
+        value={user.username}
+        placeholder="username"
+        onChange={(e) => {
+          setUser({ ...user, username: e.target.value });
+        }}
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg"
+        type="text"
+        id="email"
+        value={user.email}
+        placeholder="email"
+        onChange={(e) => {
+          setUser({ ...user, email: e.target.value });
+        }}
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg"
+        type="text"
+        id="password"
+        value={user.password}
+        placeholder="password"
+        onChange={(e) => {
+          setUser({ ...user, password: e.target.value });
+        }}
+      />
+      <button
+        className="p-2 border border-gray-300 rounded-lg-mb-4"
+        onClick={onSignup}
+      >
+        SignUp
+      </button>
+      <Link href="/login">Visit Login</Link>
+    </div>
+  );
 }
